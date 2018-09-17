ac8f3db76ac59f88841eeb99f07a1f942b1244b9
zhixi.liu <zhixi.liu@baifendian.com>
2018-09-17 19:00:02 +0800

    mysql批量导入结合数据库配置

--- app/config/index.js
+++ app/config/index.js
@@ -45,3 +45,77 @@ const configs = {
 };
 
 module.exports = configs;
+
+/*
+注释版
+  // 数据库的 sql 方言
+  // 当前已支持: 'mysql', 'sqlite', 'postgres', 'mssql'
+  dialect: 'mysql',
+  
+  // 自定义主机; 默认值: localhost
+  host: 'my.server.tld',
+  // 对于 postgres，你还可以指定包含 UNIX 套接字的目录的绝对路径，
+  // 来通过 host: '/sockets/psql_sockets' 进行连接。
+ 
+  // 自定义端口; 默认值: 依据 dialect 默认
+  port: 12345,
+ 
+  // 自定义协议，默认值: 'tcp'
+  // 仅限 postgres, 用于 Heroku
+  protocol: null,
+ 
+  // 禁用日志; 默认值: console.log
+  logging: false,
+  
+  // 你还可以将任何方言选项传递到底层方言库
+  // - 默认是空
+  // - 当前支持: 'mysql', 'postgres', 'mssql'
+  dialectOptions: {
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+    supportBigNumbers: true,
+    bigNumberStrings: true
+  },
+ 
+  // sqlite 的存储引擎
+  // - 默认值 ':memory:'
+  storage: 'path/to/database.sqlite',
+ 
+  // 禁止将未定义的值插入为NULL
+  // - 默认值: false
+  omitNull: true,
+ 
+  // 是否使用本地库的标志
+  // 如果是 'pg' -- 设置为 true 将允许 SSL 支持
+  // - 默认值: false
+  native: true,
+ 
+  // 指定在调用 sequelize.define 时使用的选项
+  // 如下示例:
+  //   define: { timestamps: false }
+  // 这基本等同于:
+  //   sequelize.define(name, attributes, { timestamps: false })
+  // 没有必要像这样去定义每个模型的时间戳选项
+  define: {
+    underscored: false
+    freezeTableName: false,
+    charset: 'utf8',
+    dialectOptions: {
+      collate: 'utf8_general_ci'
+    },
+    timestamps: true
+  },
+ 
+  // 类似于同步：你可以定义始终强制同步模型
+  sync: { force: true },
+ 
+  // 用于数据库连接池的池配置
+  pool: {
+    max: 5,
+    idle: 30000,
+    acquire: 60000,
+  },
+
+  // 每个事务的隔离级别. 
+  // 默认为 dialect 默认
+  isolationLevel: Transaction.ISOLATION_LEVELS.REPEATABLE_READ
+})*/
--- app/controllers/area.js
+++ app/controllers/area.js
@@ -53,5 +53,44 @@ module.exports = {
     }
 
     ctx.res.ok(provinces, "success");
+  },
+  batchUpdateHotTrend: async ctx => {
+    const { sequelize } = ctx.orm();
+    let { topicId, categoryId, langId, list } = ctx.request.body;
+    if (
+      // 验证参数必填项
+      !ctx.res.checkParams(ctx.request.body, [
+        "topicId",
+        "categoryId",
+        "langId",
+        "list"
+      ])
+    ) {
+      return;
+    }
+
+    //将条件组合成一个临时视图（相当于一个列名为degree和day的表格）
+    let tempTable = list
+      .map(item => {
+        return `
+      select '${item.degree}' as degree, '${item.day}' as day
+      `;
+      })
+      .join(" UNION ALL ");
+    // select 1和select 2的结果加起来，并且不处理重复项。
+    // 比如：select 1返回的是为:1,2,3，select 2返回的值为2,4,5，那么整个这句话返回的值为：1,2,3,2,4,5
+
+    //更新表tbs_dm_bs_out_topic_heat_trend_sec_screen别名a 临时表as b
+    //将b的degree批量导入给a表的all_action_count列条件是下面那个
+    let sql = `update tbs_dm_bs_out_topic_heat_trend_sec_screen a,(${tempTable}) b
+        set a.all_action_count = b.degree 
+        WHERE a.type = 'daily_topice_heat_trend_recently'
+        AND a.topic_category_id=${categoryId}
+        AND a.topic_hash=${topicId}
+        AND a.language='${langId}'
+        AND a.day= b.day`;
+    const result = await sequelize.query(sql); //sequelize执行query语句
+    console.log(result);
+    ctx.res.ok({}, "success");
   }
 };
